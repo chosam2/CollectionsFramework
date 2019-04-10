076a0f1f9df4377db946a05b76a3b5bc44b94bb4
hyungwook0221 <20140931@gm.hannam.ac.kr>
2019-04-10 10:51:02 +0900

    # 0410 컬렉션 숙

--- src/kr/or/ddit/basic/T14_PhoneBookTest.java
+++ src/kr/or/ddit/basic/T14_PhoneBookTest.java
@@ -7,19 +7,24 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap.KeySetView;
 
-public class T14_PhoneTest {
-	Scanner sc = new Scanner(System.in);
+public class T14_PhoneBookTest {
+	Scanner sc;
 	Phone phone;
-	Map<String, Phone> map = new HashMap<>();
+	Map<String, Phone> map;
+
+	public T14_PhoneBookTest() {
+		sc = new Scanner(System.in);
+		map = new HashMap<>();
+	}
 
 	public static void main(String[] args) {
-		T14_PhoneTest phoneTest = new T14_PhoneTest();
+		T14_PhoneBookTest phoneTest = new T14_PhoneBookTest();
 
-		phoneTest.programMain();
+		phoneTest.phoneBookStart();
 
 	}
 
-	void programMain() {
+	void phoneBookStart() {
 
 		System.out.println("==================================");
 		System.out.println("전화번호 관리 프로그램(파일로 저장되지 않음");
@@ -38,10 +43,10 @@ public class T14_PhoneTest {
 
 			switch (input) {
 			case 1:
-				phoneAdd();
+				phoneInsert();
 				break;
 			case 2:
-				phoneModify();
+				phoneUpdate();
 				break;
 			case 3:
 				phoneDelete();
@@ -58,6 +63,84 @@ public class T14_PhoneTest {
 		}
 	}
 
+	private void phoneInsert() {
+		System.out.println();
+		System.out.println("새롭게 등록할 전화번호 정보를 입력하세요.");
+		System.out.print("이름 >> ");
+		String name = sc.nextLine().trim();
+
+		if (map.get(name) != null) {
+			System.out.println(name + "씨는 이미 등록된 사람입니다.");
+			return;
+		}
+
+		System.out.print("전화번호 >> ");
+		//		String tel = sc.next();	 // 엔터키 포함해서 전달
+		//		sc.hasNextLine();	// 버퍼에 있는 엔터키 받아서 제거시켜줌.
+
+		String tel = sc.nextLine().trim();
+		System.out.print("주소 >> ");
+		String addr = sc.nextLine();
+
+		map.put(name, new Phone(name, tel, addr));
+		System.out.println(name + "씨 등록 완료!");
+
+	}
+
+	private void phoneDelete() {
+		System.out.println("삭제할 이름을 입력하세요.");
+		String name = sc.nextLine();
+
+		if (map.remove(name) == null) {
+			System.out.println(name + "씨는 등록되지 않은 사람입니다.");
+		} else {
+			System.out.println(name + "씨가 삭제되었습니다.");
+		}
+
+		//		Set<String> keySet = map.keySet();
+		//		for (String key : keySet) {
+		//			if (name.equals(key)) {
+		//				map.remove(key);
+		//			}
+		//		}
+	}
+
+	private void phoneUpdate() {
+		System.out.println("수정할 이름을 입력하세요.");
+		String name = sc.nextLine();
+
+		System.out.print("번호 >> ");
+		String tel = sc.nextLine().trim();
+		System.out.print("주소 >> ");
+		String addr = sc.nextLine();
+		map.put(name, new Phone(name, tel, addr));
+
+	}
+
+	/**
+	 * Iterator을 이용한 리스트 조회
+	 */
+	private void phoneSearch() {
+		System.out.println("검색할 이름을 입력하세요.");
+		String name = sc.nextLine().trim();
+
+		if (map.get(name) == null) {
+			System.out.println(name + "씨는 등록되지 않은 사람입니다.");
+			return;
+		}
+
+		Set<String> keySet = map.keySet();
+
+		Iterator<String> it = keySet.iterator();
+		while (it.hasNext()) {
+			String key = it.next();
+			if (key.equals(name)) {
+				System.out.println(key + " : " + map.get(name));
+			}
+		}
+
+	}
+
 	private void phoneShowAll() {
 		Set<Map.Entry<String, Phone>> mapSet = map.entrySet();
 
@@ -70,63 +153,6 @@ public class T14_PhoneTest {
 		}
 	}
 
-	/**
-	 * Iterator을 이용한 리스트 조회
-	 */
-	private void phoneSearch() {
-		System.out.println(map);
-		System.out.println("검색할 이름을 입력하세요.");
-		String name = sc.nextLine().trim();
-
-		Set<String> keySet = map.keySet();
-
-		Iterator<String> it = keySet.iterator();
-		while (it.hasNext()) {
-			String key = it.next();
-			if (key.equals(name)) {
-				System.out.println(key + " : " + map.get(key));
-			}
-		}
-
-	}
-
-	private void phoneDelete() {
-		System.out.println("삭제할 이름을 입력하세요.");
-		String name = sc.nextLine();
-
-		Set<String> keySet = map.keySet();
-		for(String key : keySet) {
-			if(name.equals(key)) {
-				map.remove(key);
-			}
-		}
-	}
-
-	private void phoneModify() {
-		System.out.println("수정할 이름을 입력하세요.");
-		String name = sc.nextLine();
-
-		System.out.print("번호 >> ");
-		String tel = sc.nextLine().trim();
-		System.out.print("주소 >> ");
-		String addr = sc.nextLine().trim();
-		map.put(name, new Phone(name, tel, addr));
-
-	}
-
-	private void phoneAdd() {
-		System.out.println("새롭게 등록할 전화번호 정보를 입력하세요.");
-
-		System.out.print("이름 >> ");
-		String name = sc.nextLine().trim();
-		System.out.print("전화번호 >> ");
-		String tel = sc.nextLine().trim();
-		System.out.print("주소 >> ");
-		String addr = sc.nextLine().trim();
-
-		map.put(name, new Phone(name, tel, addr));
-
-	}
 }
 
 class Phone {
@@ -143,6 +169,7 @@ class Phone {
 	}
 
 	public Phone(String name, String tel, String addr) {
+		super();
 		this.name = name;
 		this.tel = tel;
 		this.addr = addr;
